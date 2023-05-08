1ff945363f1d8dab34a2d2fc06bfa3b875c633e4
Rea <andrea.solisg@gmail.com>
2023-05-08 18:45:49 +0200

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+
+
+const About= () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>A proppos</h1>
+
+            <p>Et qui velit in consectetur cupidatat anim dolore voluptate irure reprehenderit excepteur enim. Nisi id irure incididunt do enim proident ea tempor proident sunt. Veniam elit sint ullamco aliquip ullamco.
+            </p>
+
+        </div>
+    )
+}
+
+export default About;
